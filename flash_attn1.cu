34f686892a660dadf9a47f2a1a08029ee1d6b3ca
divijdawar <2divijdawar@gmail.com>
2025-08-09 11:27:50 +0530

    fixed minor synctactical bugs

--- flash_attn1.cu
+++ flash_attn1.cu
@@ -1,4 +1,4 @@
-// The following is the implementation of flash attention as described in the paper: https://arxiv.org/abs/2205.14135
+//  he following is the implementation of flash attention as described in the paper: https://arxiv.org/abs/2205.14135
 
 #include <cuda_runtime.h> 
 #include <cuda_fp16.h> 
@@ -13,12 +13,13 @@ namespace cg = cooperative_groups;
 inline constexpr int num_elems = M / sizeof(__half); 
 
 template<int HEAD_DIM> 
-inline constexpr int Bc() {return (num_elems + 4 * HEAD_DIM - 1) / (4 * HEAD_DIM)} // block columns -> the number of key/value vectors processed per tile iteration
+inline constexpr int Bc() {return (num_elems + 4 * HEAD_DIM - 1) / (4 * HEAD_DIM);} // block columns -> the number of key/value vectors processed per tile iteration
 
 template<int HEAD_DIM> 
 inline constexpr int Br() { return std::min(Bc<HEAD_DIM>(), HEAD_DIM);} // block rows -> the number of rows of query vectors processed per tile iteration
 
 // 16b zero stores 
+// todo: update v8 to v4
 __device__ __forceinline__ void store16b( void* p) { 
     asm volatile("st.global.v8.u16 [%0], {0,0,0,0,0,0,0,0};" :: "l"(p) : "memory");
 }
@@ -30,7 +31,6 @@ __device__ void initialze(
     float* __restrict__ m,   // [N]
     int N,                   // rows
     int d,                   // active columns per row 
-    int ldo                  // leading dim (stride)
 ) { 
     const float neg_inf = -CUDART_INF_F;
     const int tid = blockIdx.x * blockDim.x + threadIdx.x; 
@@ -43,8 +43,8 @@ __device__ void initialze(
     const uint32_t zero = __float_as_uint(0.0f); 
     const uint32_t inf = __float_as_uint(-CUDART_INF_F); 
 
-    for (int i = tid; i < N; i += 4 * gridDim.x * blockDim.x);{
+    for (int i = tid; i < N; i += 4 * gridDim.x * blockDim.x){
         store16b<zero>(reinterpret_cast<void*>(l + i));
         store16b<inf>(reinterpret_cast<void*>(m + i));
     }
-}
+} 
